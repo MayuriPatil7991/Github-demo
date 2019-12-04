833797343360ebe0da4c86779bc1d2e13aebb2c5
Pooja Aher <mayuri.patil@neosofttech.com>
2019-12-04 15:45:29 +0530

    Initial Commit

---
+++ Github-demo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Github-demo
+//
+//  Created by webwerks on 04/12/19.
+//  Copyright © 2019 webwerks. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ Github-demoTests/Github_demoTests.swift
@@ -0,0 +1,34 @@
+//
+//  Github_demoTests.swift
+//  Github-demoTests
+//
+//  Created by webwerks on 04/12/19.
+//  Copyright © 2019 webwerks. All rights reserved.
+//
+
+import XCTest
+@testable import Github_demo
+
+class Github_demoTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
